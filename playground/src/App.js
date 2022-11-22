d38e2bcb83869b2c1af95cedcae4e69fb4c60c4d
Andrey Todorov <andrey.todorov@slr-holiday-service.bg>
2022-11-22 16:05:40 +0200

    fix: wip

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,12 +1,30 @@
 import React, {useState} from 'react';
-import {DateRangePicker, DateRangePickerTW, SingleDatePicker, SingleDatePickerTW} from 'date-picker';
+import {TimePicker, TimePickerTW} from 'date-picker';
 
 function App() {
-	const [startDate, setStartDate] = useState('2022-11-20')
-	const [endDate, setEndDate] = useState('2022-11-25')
+	// const [startDate, setStartDate] = useState('2022-11-20')
+	// const [endDate, setEndDate] = useState('2022-11-25')
+	const [time, setTime] = useState('10:40')
 
   return (
 		<div className='tw-flex tw-w-screen tw-justify-center tw-gap-2 tw-items-center tw-p-5'>
+
+			<TimePicker
+				value={time}
+				onChange={value => setTime(value)}
+				onClear={() => setTime('10:40')}
+				// clearable
+				onlyInput
+			/>
+
+			<TimePickerTW
+				// clearable
+				value={time}
+				onChange={value => setTime(value)}
+				onClear={() => setTime('10:40')}
+				onlyInput
+			/>
+
 			{/*<SingleDatePicker*/}
 			{/*	selection*/}
 			{/*	onChange={(startDate, endDate) => {*/}
@@ -37,39 +55,39 @@ function App() {
 			{/*	useTimepicker*/}
 			{/*/>*/}
 
-			<DateRangePicker
-				// asInput
-				// clearable
-				selection
-				// disabled
-				// label
-				onChange={(start, end) => {
-					setStartDate(start);
-					setEndDate(end);
-				}}
-				// onClear={() => setDate(null)}
-				// defaultStartDate
-				// defaultEndDate
-				startDate={startDate || null}
-				endDate={endDate || null}
-			/>
+			{/*<DateRangePicker*/}
+			{/*	// asInput*/}
+			{/*	// clearable*/}
+			{/*	selection*/}
+			{/*	// disabled*/}
+			{/*	// label*/}
+			{/*	onChange={(start, end) => {*/}
+			{/*		setStartDate(start);*/}
+			{/*		setEndDate(end);*/}
+			{/*	}}*/}
+			{/*	// onClear={() => setDate(null)}*/}
+			{/*	// defaultStartDate*/}
+			{/*	// defaultEndDate*/}
+			{/*	startDate={startDate || null}*/}
+			{/*	endDate={endDate || null}*/}
+			{/*/>*/}
 
-			<DateRangePickerTW
-				// asInput
-				// clearable
-				selection
-				// disabled
-				// label
-				onChange={(start, end) => {
-					setStartDate(start);
-					setEndDate(end);
-				}}
-				// onClear={() => setDate(null)}
-				// defaultStartDate
-				// defaultEndDate
-				startDate={startDate || null}
-				endDate={endDate || null}
-			/>
+			{/*<DateRangePickerTW*/}
+			{/*	// asInput*/}
+			{/*	// clearable*/}
+			{/*	selection*/}
+			{/*	// disabled*/}
+			{/*	// label*/}
+			{/*	onChange={(start, end) => {*/}
+			{/*		setStartDate(start);*/}
+			{/*		setEndDate(end);*/}
+			{/*	}}*/}
+			{/*	// onClear={() => setDate(null)}*/}
+			{/*	// defaultStartDate*/}
+			{/*	// defaultEndDate*/}
+			{/*	startDate={startDate || null}*/}
+			{/*	endDate={endDate || null}*/}
+			{/*/>*/}
 
 		</div>
 
--- src/Tailwind/TimePickerTW.js
+++ src/Tailwind/TimePickerTW.js
@@ -1,13 +1,12 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { Icon, Popup, Table } from 'semantic-ui-react';
+import React, {useState, Fragment} from 'react';
 import * as PropTypes from 'prop-types';
-import cx from 'classnames';
 
 import { MODE } from '../constants';
 import { formatTime } from '../ScrollTimePicker/utils';
+import {Popover, Transition} from "@headlessui/react";
 const today = new Date();
 
-function buildTable(mode, date, timeParts, callback, disabled) {
+function buildTable(mode, date, timeParts, callback, disabled, close) {
 	const isHours = mode === MODE.hours;
 	const rows = isHours ? 6 : 4;
 	const columns = isHours ? 4 : 3;
@@ -24,60 +23,43 @@ function buildTable(mode, date, timeParts, callback, disabled) {
 
 			const cellText = formatTime(cellDate);
 			const currentParts = cellText.split(':');
+			const isActive = timeParts && timeParts[isHours ? 0 : 1] === currentParts[isHours ? 0 : 1]
 			rowColumns.push((
-				<Table.Cell
-					disabled={disabled}
+				<td
 					onMouseDown={e => {
 						e.preventDefault()
 					}}
 					onClick={() => {
+						if (mode === MODE.minutes) {
+							close();
+						}
 						callback(cellText);
 					}}
 					key={`calendar-time-row-${r}-cell-${c}`}
-					className={cx('link', { active: timeParts && timeParts[isHours ? 0 : 1] === currentParts[isHours ? 0 : 1]})}>
+					className={`hover:tw-bg-gray-200 tw-py-2 tw-px-2 tw-border ${isActive ? 'tw-bg-gray-200'  : ''}`}>
 					{cellText}
-				</Table.Cell>
+				</td>
 			));
 		}
-		tableRows.push(<Table.Row key={`calendar-time-row-${r}`}>{rowColumns}</Table.Row>);
+		tableRows.push(<tr className='tw-bg-white tw-cursor-pointer' key={`calendar-time-row-${r}`}>{rowColumns}</tr>);
 	}
 
 	return (
-		<Table
-			celled
-			unstackable
-			textAlign="center"
-			className={mode.replace('s', '')}>
-			<Table.Body>
+		<table
+			className='tw-w-full tw-text-md tw-text-center tw-text-gray-500'>
+			<tbody>
 				{tableRows}
-			</Table.Body>
-		</Table>
+			</tbody>
+		</table>
 	);
 }
 
-function TimePickerTW({ clearable, disabled, size, label, placeholder, value, onChange, onClear, noMargin, onlyInput, popupOpen, ...rest }) {
-	const contextRef = useRef();
-	const inputRef = useRef();
-	const [open, togglePopup] = useState(false);
+function TimePickerTW({ clearable, disabled, label, placeholder, value, onChange, onClear, noMargin, onlyInput, popupOpen, ...rest }) {
 	const [mode, setMode] = useState(MODE.hours);
 
-	useEffect(() => {
-		if (mode === MODE.hours && open) {
-			inputRef.current.blur();
-		}
-	}, [mode, popupOpen]);
-
-	useEffect(() => {
-		if (popupOpen) {
-			inputRef.current.focus();
-		} else {
-			inputRef.current.blur();
-		}
-	}, [popupOpen]);
-
 	function handleClick(value) {
-		setMode(mode === MODE.hours ? MODE.minutes : MODE.hours);
 		onChange(value);
+		setMode(mode === MODE.hours ? MODE.minutes : MODE.hours);
 	}
 
 	const date = today;
@@ -88,52 +70,78 @@ function TimePickerTW({ clearable, disabled, size, label, placeholder, value, on
 		date.setMinutes(timeParts[1]);
 	}
 
-	const input = (
-		<div
-			ref={contextRef}
-			className={cx('ui input', { icon: clearable, [size]: size })}>
+	const Input = () => (
+		<div className='tw-flex tw-border tw-rounded tw-text-gray-500 tw-p-3 tw-font-semibold'>
 			<input
-				ref={inputRef}
 				readOnly
 				type="text"
 				{...rest}
 				value={value ?? ''}
 				placeholder={placeholder}
 				disabled={disabled}
-				onBlur={() => {
-					setMode(MODE.hours);
-					togglePopup(false);
-				}}
-				onFocus={() => togglePopup(true)}
 				onChange={() => {}}
 			/>
 			{clearable && value && (
-				<Icon
-					name="times"
-					className="link"
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					strokeWidth="1.5"
+					stroke="currentColor"
+					className="tw-w-5 tw-h-5"
 					onClick={() => onClear()}
-				/>
+				>
+					<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
+				</svg>
 			)}
 		</div>
 	);
 
 	return (
-		<>
-			<Popup
-				style={{ padding: 0 }}
-				context={contextRef}
-				className="calendar"
-				open={open}
-				popperDependencies={[mode]}
-				content={buildTable(mode, date, timeParts, handleClick, disabled)}
-			/>
-			{onlyInput ? input : (
-				<div className="ui field" style={{ margin: noMargin ? 0 : undefined }}>
-					{label && <label>{label}</label>}
-					{input}
-				</div>
-			)}
-		</>
+		<div className="tw-relative tw-p-2">
+			<Popover className="tw-relative">
+				{({ open: isOpen }) => (
+					<>
+						<Popover.Button
+							onBlur={() => {
+								setMode(MODE.hours);
+							}}
+							className={`${isOpen ? '' : 'tw-text-opacity-90'}
+                tw-group tw-inline-flex tw-items-center tw-rounded-md tw-px-4 tw-py-2 tw-text-base
+                tw-font-medium tw-text-white hover:tw-text-opacity-100 focus:tw-outline-none
+                focus-visible:tw-ring-1 focus-visible:tw-ring-white focus-visible:tw-ring-opacity-75`}
+						>
+							{onlyInput ? <Input /> : (
+								<div>
+									{label && <label>{label}</label>}
+									<Input />
+								</div>
+							)}
+						</Popover.Button>
+						<Transition
+							as={Fragment}
+							enter="tw-transition tw-ease-out tw-duration-200"
+							enterFrom="tw-opacity-0 tw-translate-y-1"
+							enterTo="tw-opacity-100 tw-translate-y-0"
+							leave="tw-transition tw-ease-in tw-duration-50"
+							leaveFrom="tw-opacity-100 tw-translate-y-0"
+							leaveTo="tw-opacity-0 tw-translate-y-1"
+						>
+							{open && (
+								<Popover.Panel
+									className="tw-absolute tw-left-1/2 tw-z-10 tw-mt-3 tw-w-screen tw-max-w-sm tw--translate-x-1/2 tw-transform tw-px-4 tw-px-2">
+									{({close, open}) => (
+										<div className="tw-overflow-hidden tw-rounded-lg tw-shadow-md tw-ring-1 tw-ring-black tw-ring-opacity-5">
+											{open && buildTable(mode, date, timeParts, handleClick, disabled, close)}
+										</div>
+									)}
+								</Popover.Panel>
+							)}
+						</Transition>
+					</>
+				)}
+			</Popover>
+		</ div>
 	)
 }
 
@@ -143,7 +151,6 @@ TimePickerTW.propTypes = {
 	noMargin: PropTypes.bool,
 	onlyInput: PropTypes.bool,
 	label: PropTypes.string,
-	size: PropTypes.oneOf(['mini', 'tiny', 'small', 'large', 'big', 'huge', 'massive']),
 	placeholder: PropTypes.string,
 	onChange: PropTypes.func,
 	onClear: PropTypes.func,
